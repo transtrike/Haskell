c7ced4b85c901356d4c8cdca856bc04f0d15c959
Viko <transtrike@gmail.com>
2020-11-08 15:12:10 +0200

    AddWithZip task completed

--- Functions/AddWithZip.hs
+++ Functions/AddWithZip.hs
@@ -2,5 +2,5 @@ main :: IO ()
 main = do
     let arr1 = [1, 2, 3] 
     let arr2  = [4, 5, 6]
-    --BROKENNN!!!
-    -- print $ foldl (+) (head arr1) (tail arr2 )
+    --         map - sum fst + snd - zip me in tuples the 2 arrays
+    print $ map (\(x, y) -> x + y) (zip arr1 arr2)
