2f6081f62f6e081aeb8d5d98d4fad45701007e20
Viko <transtrike@gmail.com>
2020-11-14 18:20:05 +0200

    CommandInterpreter working correctly

---
+++ Lists-Advanced/LambadaExpressions.hs
@@ -0,0 +1,21 @@
+dance :: [String] -> [String]
+dance arr = arr
+
+readUntilInner :: [String] -> String -> String -> [String]
+readUntilInner arr endWord danceWord = do
+    line <- getLine
+    if line == endWord
+    then arr
+    else if line == danceWord
+    then dance arr
+    else do
+        readUntilInner (arr ++ [line]) endWord danceWord
+
+readUntil :: [String]
+readUntil = 
+    readUntilInner [] "lambada" "dance"
+
+main :: IO ()
+main = do
+    -- {selector} => {selectorObject}.{property}
+    print readUntil
